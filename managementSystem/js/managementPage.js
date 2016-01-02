34a3e19c335777d5aaacee0b1e6636939ec95151
LuckyDogLee <570556057@qq.com>
2016-01-02 13:41:43 +0800

    注释、重复代码
    在 Js 文件开头添加各个函数功能的说明, 删除部分重复代码。

--- managementSystem/js/managementPage.js
+++ managementSystem/js/managementPage.js
@@ -1,3 +1,22 @@
+/*
+ * 本文件采用 “面向过程” 编程，可思考如何转换为 “面向对象” 编程以及是否有必要
+ * window.onload = function()-------页面加载时，生成文章列表页面
+ * createArticleList(topicId)-------生成文章图文列表页面
+ * lastPage(topicId)----------------分页效果：上一页
+ * nextPage(topicId)----------------分页效果：上一页
+ * changeTopic(topicId)-------------修改被选中的navButton的背景颜色, 更换不同主题的文章图文列表
+ * newArticlePage()-----------------生成发布新文章的页面
+ * addArticleModel()----------------添加文章模块
+ * articleAdding()------------------添加新文章
+ * setDisabled(flag)----------------设置文章内容是否可编辑：阅读模式/编辑模式，参数为 bool 类型: true/false
+ * getArticleDetail(topicId, id)----生成文章详情的页面，参数为文章所属话题的 id 和文章的 id
+ * editor(id)-----------------------文章详情页面编辑文章功能
+ * deleteFun(topicId, id)-----------文章详情页面删除文章功能
+ * saveFun(id)----------------------保存修改后的文章
+ * /
+
+
+
 /* 全局变量: 分页计数器counter */
 var counter = 0;
 
@@ -90,77 +109,105 @@ function changeTopic(topicId)
 /* 生成发布新文章的页面 */
 function newArticlePage()
 {
-	document.getElementById("right").innerHTML
-	= "<div id='parent'>"
-			+"<div id='child'>"
-		+"<table>"
-	+ "<tr><th colspan='2' align='center'>发布新文章</th></tr>"
-		+ "<tr><td>文章标题</td><td><input id='title' type='text'/></td></tr>"
-		+ "<tr><td>文章类型</td><td><input id='topic' type='text'/></td></tr>"
-	+ "<tr><td>文章图片</td><td><input id='img' type='text'/></td></tr>"
-	+ "<tr><td>文章概要</td><td><input type='text'/></td></tr>"
-	+ "<tr><td>文章内容</td><td><textarea id='content' type='text'/></textarea></td></tr>"
-	+ "<tr><td colspan='2' align='center'><input id='newArticleBtn' type='button' onclick='articleAdding()' value='发布' /></td></tr>"
-	+ "</table>"
-	+ "</div>"
-	+ "</div>"
-	;
+	addArticleModel();
+	var tr1 = document.getElementsByTagName("table")[0].insertRow(0);
+	var tr2 = document.getElementsByTagName("table")[0].insertRow(6);
+	tr1.innerHTML = "<th colspan='2' align='center'>发布新文章</th>";
+	tr2.innerHTML = "<td colspan='2' align='center'><input id='newArticleBtn' type='button' onclick='articleAdding()' value='发布' /></td>";
 }
 
-/* 生成文章详情的页面 */
-function getArticleDetail(topicId, id)
+/* 添加文章模块 */
+function addArticleModel()
 {
-	document.getElementById("right").innerHTML
-	= "<div id='parent'>"
-			+"<div id='child'>"
-		+"<table>"
-	+ "<tr><div id='imgBtn'><img class='imgItem' src='../img/editor.png' onclick='editor()'>"
-	+ "<img class='imgItem' src='../img/delete.png' onclick='deleteFun(" + topicId + ", " + id + ")'></div></tr>"
+	var right = document.getElementById('right');
+	right.innerHTML = "";
+	var parent = document.createElement('div');
+	var child = document.createElement('div');
+	var table = document.createElement('table');
+	right.appendChild(parent);
+	parent.appendChild(child);
+	child.appendChild(table);
+	parent.id = 'parent';
+	child.id = 'child';
+	table.innerHTML = "<table>"
 		+ "<tr><td>文章标题</td><td><input id='title' type='text'/></td></tr>"
 		+ "<tr><td>文章类型</td><td><input id='topic' type='text'/></td></tr>"
 	+ "<tr><td>文章图片</td><td><input id='img' type='text'/></td></tr>"
-	+ "<tr><td>文章概要</td><td><input id='summary' type='text'/></td></tr>"
+	+ "<tr><td>文章概要</td><td><input id='summary'type='text'/></td></tr>"
 	+ "<tr><td>文章内容</td><td><textarea id='content' type='text'/></textarea></td></tr>"
 	+ "</table>"
-	+ "</div>"
-	+ "</div>"
-	;
+}
 
-	document.getElementsByTagName("input")[0].setAttribute("disabled", "");
-	document.getElementsByTagName("input")[1].setAttribute("disabled", "");
-	document.getElementsByTagName("input")[2].setAttribute("disabled", "");
-	document.getElementsByTagName("input")[3].setAttribute("disabled", "");
-	document.getElementsByTagName("textarea")[0].setAttribute("disabled", "");
+/* 添加新文章 */
+function articleAdding()
+{
+
+}
+
+/* 设置文章内容是否可编辑：阅读模式/编辑模式，参数为 bool 类型: true/false */
+function setDisabled(flag)
+{
+	var inputItems = document.getElementsByTagName("input");
+	for (var i = inputItems.length - 1; i >= 0; i--) {
+		inputItems[i].disabled = flag;
+	};
+
+	document.getElementsByTagName("textarea")[0].disabled = flag;
+}
+
+/* 生成文章详情的页面，参数为文章所属话题的 id 和文章的 id */
+function getArticleDetail(topicId, id)
+{
+	addArticleModel();
+	var tr1 = document.getElementsByTagName("table")[0].insertRow(0);
+	tr1.innerHTML = "<div id='imgBtn'><img class='imgItem' src='../img/editor.png' onclick='editor(" + id + ")'>"
+	+ "<img class='imgItem' src='../img/delete.png' onclick='deleteFun(" + topicId + ", " + id + ")'></div>";
+	
 
 	document.getElementById("title").value = "Topic " + topicId + " 第" + id + "篇文章的标题";
 	document.getElementById("topic").value = "Topic " + topicId + " 第" + id + "篇文章的类型";
 	document.getElementById("img").value = "Topic " + topicId + " 第" + id + "篇文章的图片";
 	document.getElementById("summary").value = "Topic " + topicId + " 第" + id + "篇文章的概要";
 	document.getElementById("content").value = "Topic " + topicId + " 第" + id + "篇文章的内容";
+
+	/* 设置文章为“不可编辑”：阅读模式 */
+	setDisabled(true);
 }
 
 /* 文章详情页面编辑文章功能 */
-function editor()
+function editor(id)
 {
-	document.getElementsByTagName("input")[0].disabled = false;
-	document.getElementsByTagName("input")[1].disabled = false;
-	document.getElementsByTagName("input")[2].disabled = false;
-	document.getElementsByTagName("input")[3].disabled = false;
-	document.getElementsByTagName("textarea")[0].disabled = false;
+	/* 设置文章为“可编辑”模式：编辑模式 */
+	setDisabled(false);
 
+	/* 添加“保存”按钮 */
 	var tableRow = document.getElementsByTagName("table")[0].insertRow(6);
 	var flag = document.getElementById("saveBtn");
 	if (flag == null) {
-		tableRow.innerHTML = "<td colspan='2' align='center'><input id='saveBtn' type='button' onclick='articleModifying()' value='保存' /></td>"
+		tableRow.innerHTML = "<td colspan='2' align='center'><input id='saveBtn' type='button' onclick='saveFun(" + id + ")' value='保存' /></td>"
 	};
 }
 
 /* 文章详情页面删除文章功能 */
 function deleteFun(topicId, id)
 {
-	/* Todo: 删除数据库中对应 id 的文章 */
+	/* Todo: 删除数据库中对应 id 的文章: articleRemoving(id) */
 
 	/* 返回(生成)相对应 topic 的文章列表页 */
 	counter = 1;
 	createArticleList(topicId);
+}
+
+/* 保存修改后的文章 */
+function saveFun(id)
+{
+	/* Todo: 获取修改后的文章内容：getArticleContent() */
+
+	/* Todo: 同步数据库中对应文章的内容: articleModifying(id) */
+
+	/* 设置文章为“不可编辑”：阅读模式 */
+	setDisabled(true);
+
+	/* 删除“保存”按钮 */
+	document.getElementsByTagName("table")[0].deleteRow(6);
 }
